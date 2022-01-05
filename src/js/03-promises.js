7b840937ef6a04001b8d052c9e592bbd9a72f7ab
Den4icGuti <goos7412@gmail.com>
2022-01-05 14:27:40 +0200

    Сделана дз № 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from "notiflix";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit',onFormSubmit)
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  let delay = Number(e.currentTarget.delay.value);
+  let step = Number(e.currentTarget.step.value);
+  let amount = Number(e.currentTarget.amount.value);
+
+  for (let position = 0; position < amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position,delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay)
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    step += delay;
+  }
+
+
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+ 
+    const valueP = { position, delay };
+    return new Promise((resolve, reject) => {
+      if (shouldResolve) {
+        resolve(valueP)
+      } else {
+        reject(valueP)
+      }
+    })
+
   }
 }
+
+
+
+
