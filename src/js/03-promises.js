5df2f34a4f76afa3de646d291c32ab25629a0eda
Den4icGuti <goos7412@gmail.com>
2022-01-07 11:15:12 +0200

    add debug

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,7 +10,7 @@ function onFormSubmit(e) {
   let step = Number(e.currentTarget.step.value);
   let amount = Number(e.currentTarget.amount.value);
 
-  for (let position = 1; position < amount; position += 1) {
+  for (let position = 1; position <= amount; position += 1) {
     createPromise( position, delay )
       .then(({ position,delay }) => {
         setTimeout(() => {
