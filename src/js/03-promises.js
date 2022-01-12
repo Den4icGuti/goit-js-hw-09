96bacac76b4246b620d245f7acfcbbcf1d0a11e9
Den4icGuti <goos7412@gmail.com>
2022-01-12 08:10:53 +0200

    Добавлены правки

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,7 +11,7 @@ function onFormSubmit(e) {
   let amount = Number(e.currentTarget.amount.value);
 
   for (let position = 1; position <= amount; position += 1) {
-    createPromise( position, delay )
+    createPromise(position, delay)
       .then(({ position,delay }) => {
         setTimeout(() => {
           Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
@@ -20,16 +20,16 @@ function onFormSubmit(e) {
       .catch(({ position, delay }) => {
         setTimeout(() => { 
            Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        })
+        },delay)
       });
-    step += delay;
+    delay += step;
   }
 }
 
 function createPromise(position, delay) {
     const shouldResolve = Math.random() > 0.3;
  
-    const valueP = { position, delay };
+    const valueP = {position,delay};
     return new Promise((resolve, reject) => {
       if (shouldResolve) {
         resolve(valueP)
